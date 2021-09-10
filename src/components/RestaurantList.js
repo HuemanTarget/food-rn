2a4af02bc99ea3156f951920d8f5c8a6a215f91f
Joshua Basche <joshbasche@live.com>
2021-09-10 13:58:23 -0700

    create SearchBar and RestaurantList components

---
+++ src/components/RestaurantList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const RestaurantList = () => {
+  return (
+    <View>
+      <Text>Restaurant List</Text>
+    </View>
+  );
+};
+
+export default RestaurantList;
+
+const styles = StyleSheet.create({});
---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+
+const SearchBar = () => {
+  return (
+    <View>
+      <Text>Search Bar</Text>
+    </View>
+  );
+};
+
+export default SearchBar;
+
+const styles = StyleSheet.create({});
